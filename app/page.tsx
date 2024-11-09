b4aad9ba4cec3f6055aca1a55085a393c0ae83f8
themorganfamily <tim.kerriosn@gmail.com>
2024-11-09 19:51:30 +1100

    Chapter 10 complete

--- app/page.tsx
+++ app/page.tsx
@@ -18,17 +18,16 @@ export default function Page() {
           /> */}
           {/* <div className={styles.shape} /> */}
           <p className={`${lusitana.className} antialiased text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to your boilerplate.</strong> This {' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            NEXT.js   </a>
+               solution will help guide you in building apps for this platform.
           </p>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
