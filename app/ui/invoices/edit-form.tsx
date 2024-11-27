e51443efe661e8525c93bc542b4f54adfb81e091
themorganfamily <tim.kerriosn@gmail.com>
2024-11-27 12:13:16 +1100

    Fix build error on edit order page + adjust edit page for authoriosed orders

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -12,7 +12,7 @@ import {
   ReceiptRefundIcon
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { Button} from '@/app/ui/button';
+import { Button } from '@/app/ui/button';
 import { SpinnerButton } from "../SpinnerButton"
 
 import { useState, useEffect } from "react"
@@ -46,27 +46,36 @@ export default function EditInvoiceForm({
     // disableLoading();
     setIsLoading(false);
     let returnState: any;
-    if(invoice.status ==="authorised") {
+    if (invoice.status === "authorised") {
       returnState = await updateInvoiceWithId(prevState, formData);
       return returnState;
     }
-    else if(formData.get("status") ==="deleted") {
+    else if (formData.get("status") === "deleted") {
       await oldDeleteInvoice(invoice.id);
     }
     else {
-      
+
       returnState = await deleteInvoice(invoice.id, prevState, formData);
       return returnState;
-     // window.location.replace('/dashboard/invoices');
+      // window.location.replace('/dashboard/invoices');
     }
-    
+
 
   }
 
   const [state, formAction] = useActionState(serverSubmit, initialState);
 
+  var inputAmount = 0;
+
+  if (invoice.status === 'authorised') {
+    inputAmount = invoice.amount;
+  }
+  else {
+    inputAmount = invoice.captured_amount;
+  }
+
   // const [state, formAction] = useActionState(serverSubmit, initialState);
-  
+
   // const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
   return (
     <form action={formAction} aria-describedby="form-error">
@@ -104,12 +113,12 @@ export default function EditInvoiceForm({
           </label>
           <div className="relative mt-2 rounded-md">
             <div className="relative">
-              <input 
+              <input
                 id="amount"
                 name="amount"
                 type="number"
                 step="0.01"
-                defaultValue={invoice.captured_amount}
+                defaultValue={inputAmount}
                 placeholder="Enter AUD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 aria-describedby="amount-error"
@@ -135,7 +144,7 @@ export default function EditInvoiceForm({
           <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
             <div className="flex gap-4">
               <div>
-                {invoice.status === 'authorised' &&
+                {invoice.status === 'authorised' ?
                   <div className="flex gap-4">
                     <div className="flex items-center">
                       <input
@@ -171,8 +180,7 @@ export default function EditInvoiceForm({
                       </label>
 
                     </div>
-                  </div>}
-                  {invoice.status === 'partially refunded'  &&
+                  </div> :
                   <div>
                     <div className="flex items-center">
                       <input
@@ -190,75 +198,30 @@ export default function EditInvoiceForm({
                       >
                         Refund <ReceiptRefundIcon className="h-4 w-4" />
                       </label>
-                    </div>   
-                    </div>   
-                }
-                {invoice.status === 'refunded'  &&
-                  <div>
-                    <div className="flex items-center">
-                      <input
-                        id="deleted"
-                        name="status"
-                        type="radio"
-                        value="deleted"
-                        defaultChecked={true}
-                        className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
-                        aria-describedby="status-error"
-                      />
-                      <label
-                        htmlFor="deleted"
-                        className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full px-3 py-1.5 text-xs font-medium bg-red-100"
-                      >
-                        Delete <XMarkIcon className="h-4 w-4" />
-                      </label>
-                    </div>   
-                    </div>   
-                }
-                {invoice.status === 'captured'  &&
-                  <div>
-                    <div className="flex items-center">
-                      <input
-                        id="refunded"
-                        name="status"
-                        type="radio"
-                        value="refunded"
-                        defaultChecked={true}
-                        className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
-                        aria-describedby="status-error"
-                      />
-                      <label
-                        htmlFor="refunded"
-                        className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full px-3 py-1.5 text-xs font-medium bg-red-100"
-                      >
-                        Refund <XMarkIcon className="h-4 w-4" />
-                      </label>
-                    </div>   
-                    </div>   
-                }
+                    </div>
                   </div>
 
-
-
-           
-                  
-
+                }
               </div>
 
-            </div>
-            <div id="status-error" aria-live="polite" aria-atomic="true">
-              {state.errors?.status &&
-                state.errors.status.map((error: string) => (
-                  <p className="mt-2 text-sm text-red-500" key={error}>
-                    {error}
-                  </p>
-                ))}
+
             </div>
 
-            <div id="form-error" aria-live="polite" aria-atomic="true">
-              <p className="mt-2 text-sm text-red-500">
-                {state.message}
-              </p>
-            </div>
+          </div>
+          <div id="status-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.status &&
+              state.errors.status.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
+
+          <div id="form-error" aria-live="polite" aria-atomic="true">
+            <p className="mt-2 text-sm text-red-500">
+              {state.message}
+            </p>
+          </div>
         </fieldset>
       </div>
       <div className="mt-6 flex justify-end gap-4">
