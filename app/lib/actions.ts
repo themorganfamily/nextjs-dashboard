b028fcc91c80200a4645763266042f572a9dcd5f
tim <timothy.kerrison@zip.co>
2025-01-11 08:51:30 +1100

    url

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -15,7 +15,7 @@ import { UUID } from 'crypto';
 
 // export const maxDuration = 60;
 
-const baseUrl = "https://wt3x47mq.aue.devtunnels.ms:3001"
+const baseUrl = "https://6xj0tkb6.aue.devtunnels.ms:3001"
 
 export async function getServerSideProps(context:any) {
     console.log(context.req.headers.referer)
